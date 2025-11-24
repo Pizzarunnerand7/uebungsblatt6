525326cea3910580acc6f557cf3dcc7c3da1d7ca
Pizzarunnerand7 <chrisalme@gmx.net>
2025-11-24 12:04:20 +0100

    2.commit

---
+++ src/h1/H1_main.java
@@ -0,0 +1,24 @@
+package h1;
+
+public class H1_main {
+
+	public static void main(String[] args) {
+
+	}
+	public static boolean isMirrorArray(int[]a,int[]b) {
+		if (a.length != b.length) {
+			return false;
+		}
+		else {
+			for (int i=0; i<a.length; i++) {
+				if(a[i]!=b[b.length-i-1]) {
+					return false;
+				}
+				
+			}
+			return true;
+		}
+		
+	}
+
+}
