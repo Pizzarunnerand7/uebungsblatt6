b5fc326c6ce4700a9e5b9d911f2d482194037d81
Pizzarunnerand7 <chrisalme@gmx.net>
2025-11-24 12:10:24 +0100

    3.commit

---
+++ src/h2/H2_main.java
@@ -0,0 +1,23 @@
+package h2;
+
+public class H2_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	public static boolean compareArrays(int[]a,int[]b) {
+		if(a.length!=b.length) {
+			return false;
+		}
+		else {
+			for(int i=0; i<a.length; i++) {
+				if(a[i]!=b[i]) {
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+
+}
